a134b7f5878d404ab9f39ffd3ec3afd4ccdc91ef
yahhamdii <hamdi_yahyaoui@yahoo.fr>
2023-03-11 20:05:28 +0100

    create post and crud with postgresql

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { Pool } = require('pg');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const pool = new Pool({
+  user: 'root',
+  host: 'localhost',
+  database: 'expressapp',
+  password: 'root',
+  port: 5432,
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// GET all users
+app.get('/users', async (req, res) => {
+  const { rows } = await pool.query('SELECT * FROM users');
+  res.send(rows);
+});
+
+// POST a new user
+app.post('/users', async (req, res) => {
+
+  console.log(req.body);
+  const { name, email } = req.body;
+  await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+  res.send('User added successfully');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
